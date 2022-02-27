174bb5677066278d3d9128b8c148ee93b09656b2
Amiad <402108+amiad@users.noreply.github.com>
2022-02-27 16:23:20 +0200

    fix missing Yud & mb_pos, mb_str_split

--- gematria.php
+++ gematria.php
@@ -4,21 +4,21 @@ namespace Gematria;
 class Gematria {
     const LETTER_VALUES = [
         1 => 'אבגדהוזחט',
-        10 => 'כלמנסעפצ',
+        10 => 'יכלמנסעפצ',
         100 => 'קרשת',
     ];
     const FINAL_LETTERS = 'םןץפך';
     
     function __construct($text) {
         $sum = 0;
-        foreach (str_split($text) as $letter){
+        foreach (mb_str_split($text) as $letter){
             if (strpos(SELF::FINAL_LETTERS, $letter) !== false){
                 $letter = mb_chr(mb_ord($letter) + 1); // convert final letter to regular
             }
             foreach (SELF::LETTER_VALUES as $value => $letters){
-                $postion = strpos($letters, $letter);
-                if ($postion !== false){
-                    $sum += ($postion + 1) * $value;
+                $position = mb_strpos($letters, $letter);
+                if ($position !== false){
+                    $sum += ($position + 1) * $value;
                     break;
                 }
             }
